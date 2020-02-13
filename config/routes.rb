947a53435f0c8bf209bbc9c239c78f5075d1fe4d
takekou <takekou0130@gmail.com>
2020-02-13 17:35:54 +0900

    create new create method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  # postsのルーティング
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
+  
+  # homeのルーティング
   get "/" => "home#top"
   get "info" => "home#info"
+  
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
