0fa535a98e6bcf0fa7e1da277f4b1b89be35d016
takekou <takekou0130@gmail.com>
2020-02-14 00:03:02 +0900

    Create post edit destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,34 @@
 class PostsController < ApplicationController
   def index
-    @posts=Post.all
+    @posts=Post.all.order(updated_at: :desc)
   end
+
   def new
   end
+
   def create
     @post=Post.new(content: params[:content])
     @post.save
     redirect_to("/posts/index")
   end
+
+  def show
+    @post=Post.find_by(id: params[:id])
+  end
+  def destroy
+    @post=Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
+  def edit
+    @post=Post.find_by(id: params[:id])
+  end
+  def update
+    @post=Post.find_by(id: params[:id])
+    @post.content=params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # postsのルーティング
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
+  post "posts/:id/update" => "posts#update"
   post "posts/create" => "posts#create"
+  get "posts/:id/destroy" => "posts#destroy" 
+  # ↑これはpostじゃなくていい？
+  get "posts/:id/edit" => "posts#edit"
+  get "posts/:id" => "posts#show"
   
   # homeのルーティング
   get "/" => "home#top"
